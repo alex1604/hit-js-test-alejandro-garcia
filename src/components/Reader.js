8101e75871ca789d4ae8a0426224575225e92a35
Alejandro <92.alejandrogarcia@gmail.com>
2019-04-03 23:44:50 +0200

    add library for parsing string into html and map through apis data ti display papers

--- src/components/Reader.js
+++ src/components/Reader.js
@@ -1,32 +1,51 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
+import ReactHtmlParser from 'react-html-parser';
 
 class Reader extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            whitepapers: undefined
+            whitepapers: []
         }
+        this.parser = new DOMParser()
     }
-  componentDidMount() {
-      fetch("https://humanit.se/wp-json/wp/v2/whitepaper", {  // fetch whitepapers from API
-          method: 'GET'
-      })
-      .then(response => response.json()) // 
-      .then(data => {
-          this.setState({whitepapers: data})
-          console.log(this.state.whitepapers)
-      })
-      .catch(err => {
-          // handle error
-      })
-  }
-  render() {
-    return (
-      <div className="readerContainer">
-    
-      </div>
-    );
-  }
+    componentDidMount() {
+        fetch("https://humanit.se/wp-json/wp/v2/whitepaper", {  // fetch whitepapers from API
+            method: 'GET'
+        })
+            .then(response => response.json()) // 
+            .then(data => {
+                this.setState({ whitepapers: data })
+            })
+            .catch(err => {
+                // handle error
+            })
+    }
+    render() {
+        
+        return (
+            <ReaderContainer>
+                {
+                    this.state.whitepapers !== undefined ?
+                        this.state.whitepapers.map(paper => {
+                            return (
+                            <div key={paper.id}>
+                                <h4>{paper.title.rendered}</h4>
+                                {ReactHtmlParser(paper.content.rendered)}
+                            </div>
+                            )
+                        })
+                        : null
+                }
+            </ReaderContainer>
+        );
+    }
 }
+
+const ReaderContainer = styled.div`
+  height: 10vh;
+  width: 96vw;
+`
 
 export default Reader;
