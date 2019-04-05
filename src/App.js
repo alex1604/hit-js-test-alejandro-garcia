057544c050b03a3ae24f9dd81aa3d2998ab3d3bc
Alejandro <92.alejandrogarcia@gmail.com>
2019-04-05 12:26:09 +0200

    run eslint fix

--- src/App.js
+++ src/App.js
@@ -1,56 +1,58 @@
 import React, { Component } from 'react';
-import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 import Reader from './components/Reader';
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      loadedPapers: false
-    }
-    this.addPdf = this.addPdf.bind(this)
-    this.getDownloadLink = this.getDownloadLink.bind(this)
+      loadedPapers: false,
+    };
+    this.addPdf = this.addPdf.bind(this);
+    this.getDownloadLink = this.getDownloadLink.bind(this);
   }
 
   async getDownloadLink(links) {
-    const attachmentUri = links["wp:attachment"][0].href
+    const attachmentUri = links['wp:attachment'][0].href;
     if (attachmentUri === undefined) {
-      return -1
+      return -1;
     }
     const response = await fetch(attachmentUri);
-    const body = await response.body.getReader().read()
-    const decoded = await String.fromCharCode(...new Uint8Array(body.value))
-    const parsed = await JSON.parse(decoded)
-    const pdfUri = parsed[0] !== undefined ? await parsed[0].source_url : undefined
-    return pdfUri
+    const body = await response.body.getReader().read();
+    const decoded = await String.fromCharCode(...new Uint8Array(body.value));
+    const parsed = await JSON.parse(decoded);
+    const pdfUri = parsed[0] !== undefined ? await parsed[0].source_url : undefined;
+    return pdfUri;
   }
 
-  addPdf (papers) {
-    var self = this
-    let promises = papers.map(paper => new Promise(function(resolve, reject) {
-       // ett promise för varje download link
-       self.getDownloadLink(paper._links).then(function(res) {
-          paper.downloadLink = res;
-          resolve();  // tala om att detta promise är färdigt
-       });
-    }))
-    return Promise.all(promises)  // returna något som vi kan skriva await framför
+  addPdf(papers) {
+    const self = this;
+    const promises = papers.map(paper => new Promise(((resolve, reject) => {
+      // ett promise för varje download link
+      self.getDownloadLink(paper._links).then((res) => {
+        paper.downloadLink = res;
+        resolve(); // tala om att detta promise är färdigt
+      }).catch((err) => {
+        reject(err);
+      });
+    })));
+    return Promise.all(promises); // returna något som vi kan skriva await framför
   }
 
   async componentDidMount() {
     const response = await fetch('https://humanit.se/wp-json/wp/v2/whitepaper');
-    let json = await response.json();
-    await this.addPdf(json)
-    this.setState({ whitepapers: json}, () => {
-      this.setState({loadedPapers: true})
-    })
+    const json = await response.json();
+    await this.addPdf(json);
+    this.setState({ whitepapers: json }, () => {
+      this.setState({ loadedPapers: true });
+    });
   }
- 
+
   render() {
-    let reader = this.state.loadedPapers !== false ? (
-      <Reader whitepapers={this.state.whitepapers}/>
-    ) : null
+    const reader = this.state.loadedPapers !== false ? (
+      <Reader whitepapers={this.state.whitepapers} />
+    ) : null;
     return (
       <div className="App">
         <header className="App-header">
